c27a0f610d6beae4720d8c9e61dceedacf621514
Yurii Chalov <yuriy dot chalov 03 at gmail dot com>
2024-01-06 16:27:02 -0500

    update comments, optimize and refactored code

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFLEN 12 
+#define BUFLEN 256
 
 const char* str_str(const char* pattern, const char* str, int len)
 {
@@ -38,26 +38,27 @@ int find_substring(FILE *f, const char *pattern)
     int res = 0;
     int pattern_len = str_len(pattern);
     int read_len;
-    int tmp_buf_read = 1;
-    char* tmp_buf = malloc(pattern_len * 2 - 1);
-    char* buf = malloc(BUFLEN + 1);
-    while ((read_len = fread(buf, sizeof(char), BUFLEN, f)) != 0) {
-        buf[read_len] = 0;
-        if (tmp_buf_read) {
-            int len = read_len > pattern_len - 1 ? pattern_len - 1 : read_len;
-            mem_move(tmp_buf, buf + read_len - len, len);
-            tmp_buf[len] = 0;
+    int read_at_end = 1;
+    char* inter_buf = malloc(pattern_len * 2 - 1);
+    char* main_buf = malloc(BUFLEN + 1);
+    while ((read_len = fread(main_buf, sizeof(char), BUFLEN, f)) != 0) {
+        main_buf[read_len] = 0;
+        int len = read_len > pattern_len - 1 ? pattern_len - 1 : read_len;
+        if (read_at_end) {
+            mem_move(inter_buf, main_buf + read_len - len, len);
+            inter_buf[len] = 0;
         } else {
-            int tmp_buf_len = str_len(tmp_buf);
-            int len = read_len > pattern_len - 1 ? pattern_len - 1 : read_len;
-            mem_move(tmp_buf + tmp_buf_len, buf, len);
-            tmp_buf[len + tmp_buf_len] = 0;
-            if (str_str(pattern, tmp_buf, pattern_len * 2 - 1))
-                res++;
+            int inter_buf_len = str_len(inter_buf);
+            mem_move(inter_buf + inter_buf_len, main_buf, len);
+            inter_buf[len + inter_buf_len] = 0;
+            if (str_str(pattern, inter_buf, pattern_len * 2 - 1))
+                res++;             /* we wrote to the first slice of buffer
+                                      and have already written to the second,
+                                      so we can find substring */
         }
-        tmp_buf_read ^= 1;
-        const char *tmp = buf;
-        while ((tmp = str_str(pattern, tmp, read_len - (tmp - buf)))
+        read_at_end ^= 1;
+        const char *tmp = main_buf;
+        while ((tmp = str_str(pattern, tmp, read_len - (tmp - main_buf)))
                 != NULL) {
             res++;
             tmp += pattern_len;
@@ -67,27 +68,27 @@ int find_substring(FILE *f, const char *pattern)
         fprintf(stderr, "error while reading file\n");
         perror("fread");
     }
-    free(tmp_buf);
-    free(buf);
+    free(inter_buf);
+    free(main_buf);
     return res;
 }
 
 int main(int argc, const char** argv)
 {
-	if (argc < 3) {
-		fprintf(stderr, "usage: <substring> <file>\n");
-		return 1;
-	}
+    if (argc < 3) {
+        fprintf(stderr, "usage: <substring> <file>\n");
+        return 1;
+    }
 
-	FILE *f;
+    FILE *f;
     f = fopen(argv[2], "r");
-	if (!f) {
-		fprintf(stderr, "unable to open a file\n");
-		perror("fopen");
-		return 2;
-	}
-	int res = find_substring(f, argv[1]); 
-	fclose(f);
+    if (!f) {
+        fprintf(stderr, "unable to open a file\n");
+        perror("fopen");
+        return 2;
+    }
+    int res = find_substring(f, argv[1]);
+    fclose(f);
     /*
         process arguments
         usage: [args] <pattern> [file]
@@ -96,11 +97,6 @@ int main(int argc, const char** argv)
 
         open a main program with arguments and processing a pattern
         print string with pattern
-    */
-    /*
-        open a file
-        find substring in a file
-        print count of matched substrings in a file
     */
     printf("%d\n", res);
     return 0;
